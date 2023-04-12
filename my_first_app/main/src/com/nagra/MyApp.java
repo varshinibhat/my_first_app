933543dd6a7fa69613d4d288cf91762bcd5ea04e
rouiller <bastien.rouiller@nagra.com>
2023-04-12 16:17:53 +0530

    Fix remaining issues with code

--- my_first_app/main/src/com/nagra/MyApp.java
+++ my_first_app/main/src/com/nagra/MyApp.java
@@ -2,6 +2,6 @@ package com.nagra.myapp;
 
 public class MyApp {
     public static void main(String[] args) {
-        System.out.println("Hello world! This is Bastien.");
+        System.out.println("Hello world!");
     }
 }
