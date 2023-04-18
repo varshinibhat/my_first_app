83551aec4992c467af6b75c383a9134ca16078a8
Varshini Bhat <varshinibhat65@gmail.com>
2023-04-18 12:31:57 +0530

    updated hello program

--- my_first_app/main/src/com/nagra/MyApp.java
+++ my_first_app/main/src/com/nagra/MyApp.java
@@ -1,7 +1,8 @@
 package com.nagra;
 
 public class MyApp {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world! This is Varshini");
     }
 }
